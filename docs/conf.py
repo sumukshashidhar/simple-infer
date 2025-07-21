3fceb21817bf4be69098214ffde13cb05eeed8ad
Sumuk Shashidhar <sumukuuu@gmail.com>
2025-07-21 14:30:16 -0700

    done

--- docs/conf.py
+++ docs/conf.py
@@ -47,4 +47,4 @@ napoleon_include_private_with_doc = False
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+# html_static_path = ['_static']  # Disabled - empty directory causes warnings
